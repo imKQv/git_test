902a7b1e4c7f66814579bff34dcc4cb71eaf8589
孔泉 <93875726+imKQv@users.noreply.github.com>
2022-07-06 16:19:00 +0800

    remote test

--- src/main/java/com/atguigu/git.java
+++ src/main/java/com/atguigu/git.java
@@ -8,5 +8,6 @@ public class git {
         System.out.println("pull test");
         System.out.println("push test2");
         System.out.println("pull test2");
+        System.out.println("remote test");
     }
 }
