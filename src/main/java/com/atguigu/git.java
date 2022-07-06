996859d9b3f2feaf35a698e1e27f58a6da797e33
KQ <1969676821@qq.com>
2022-07-06 16:07:10 +0800

    push commit

--- src/main/java/com/atguigu/git.java
+++ src/main/java/com/atguigu/git.java
@@ -6,5 +6,6 @@ public class git {
         System.out.println("Test 2");
         System.out.println("push test");
         System.out.println("pull test");
+        System.out.println("push test2");
     }
 }
