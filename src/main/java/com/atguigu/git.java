be2845cc6d1c88d801fca0c9917c13c30aebc155
KQ <1969676821@qq.com>
2022-07-06 16:19:46 +0800

    local commit

--- src/main/java/com/atguigu/git.java
+++ src/main/java/com/atguigu/git.java
@@ -11,5 +11,6 @@ public class git {
         System.out.println("hot-fix test");
         System.out.println("master test");
         System.out.println("hot-fix test2");
+        System.out.println("local test");
     }
 }
